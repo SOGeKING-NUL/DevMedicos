19b45bd79c649d052d7eed2537ba9336614420a4
Utsav Jana <utsav.jana.5@gmail.com>
2024-11-07 01:55:43 +0530

    inventory schema decided

--- data/db_schema.js
+++ data/db_schema.js
@@ -11,63 +11,75 @@ const db=  new sqlite.Database("./DevMedicos.db", (err)=>{
 
 
 const items="CREATE TABLE IF NOT EXISTS items(\
-            id INTEGER PRIMARY KEY AUTOINCREMENT,\
-            item TEXT NOT NULL\
+            item TEXT PRIMARY KEY\
             )"
+
 const inventory="CREATE TABLE IF NOT EXISTS inventory (\
                 id INTEGER PRIMARY KEY AUTOINCREMENT,\
                 item TEXT NOT NULL,\
-                quantity INTEGER NOT NULL,\
+                rate_per_unit DECIMAL(7,2) NOT NULL CHECK (rate_per_unit >= 0),\
+                mrp_per_unit DECIMAL(7,2) NOT NULL CHECK (mrp_per_unit >= 0),\
+                FOREIGN KEY(item) REFERENCES items (item)\
                 )"
 
 const shipment= "CREATE TABLE IF NOT EXISTS shipment (\
-                id INTEGER PRIMARY KEY,\
-                created_on DATE DEFAULT CURRENT_DATE,\
-                quantity DECIMAL(10,2) NOT NULL,\
-                bonus DECIMAL(10,2),\
+                invoice_no TEXT PRIMARY KEY,\
+                created_on DATE NOT NULL DEFAULT CURRENT_DATE,\
+                quantity INTEGER NOT NULL CHECK (quantity >= 0),\
+                bonus INTEGER CHECK (bonus >= 0),\
+                pack_unit TEXT CHECK (pack_unit IN ('TAB', 'CAP', 'ML', 'GM')),\
                 item TEXT NOT NULL,\
-                exp_date NOT NULL TEXT,\
-                mrp NOT NULL DECIMAL()10,2,\
-                rate NOT NULL DECIMAL(10,2),\
-                amount NOT NULL DECIMAL(10,2)\
+                mrp DECIMAL(7,2) NOT NULL CHECK (mrp >= 0) ,\
+                rate DECIMAL(7,2) NOT NULL CHECK (rate >= 0),\
+                amount DECIMAL(10,2) NOT NULL CHECK (amount >= 0),\
+                FOREIGN KEY(item) REFERENCES items (item)\
                 )"
 
-const bill= "CREATE TABLE IF NOT EXISTS bill (\
-            bill_no INTEGER PRIMARY KEY AUTOINCREMENT,\
-            created_on DATE DEFAULT CURRENT_DATE,\
-            total_bill_amount DECIMAL(10,2) NOT NULL\
-            )"
+// const bill= "CREATE TABLE IF NOT EXISTS bill (\
+//             bill_no INTEGER PRIMARY KEY AUTOINCREMENT,\
+//             created_on DATE DEFAULT CURRENT_DATE,\
+//             total_bill_amount DECIMAL(10,2) NOT NULL\
+//             )"
 
-const bill_item="CREATE TABLE IF NOT EXISTS bill_item(\
-                id INTEGER PRIMARY KEY AUTOINCREMENT,\
-                bill_no INTEGER,\
-                inventory_id INTEGER,\
-                quantity INTEGER NOT NULL,\
-                total_price DECIMAL(10,2) NOT NULL,\
-                FOREIGN KEY (bill_no) REFERENCES bill(bill_no),\
-                FOREIGN KEY(inventory_id) REFERENCES inventory (id)\
-                )"
+// const bill_item="CREATE TABLE IF NOT EXISTS bill_item(\
+//                 id INTEGER PRIMARY KEY AUTOINCREMENT,\
+//                 bill_no INTEGER,\
+//                 inventory_id INTEGER,\
+//                 quantity INTEGER NOT NULL,\
+//                 total_price DECIMAL(10,2) NOT NULL,\
+//                 FOREIGN KEY (bill_no) REFERENCES bill(bill_no),\
+//                 FOREIGN KEY(id) REFERENCES inventory(id)\
+//                 )"
 
 
 
+db.run(items, (err) => {
+    if (err) {
+        console.error("Error creating items table:", err.message);
+    }
+})
 
 db.run(inventory, (err) => {
     if (err) {
         console.error("Error creating inventory table:", err.message);
     }
 });
+
 db.run(shipment, (err) => {
     if (err) {
         console.error("Error creating shipment table:", err.message);
     }
 });
-db.run(bill, (err) => {
-    if (err) {
-        console.error("Error creating bill table:", err.message);
-    }
-});
-db.run(bill_item, (err) => {
-    if (err) {
-        console.error("Error creating bill_item table:", err.message);
-    }
-});
+
+
+
+// db.run(bill, (err) => {
+//     if (err) {
+//         console.error("Error creating bill table:", err.message);
+//     }
+// });
+// db.run(bill_item, (err) => {
+//     if (err) {
+//         console.error("Error creating bill_item table:", err.message);
+//     }
+// });
--- server/server.js
+++ server/server.js
@@ -1,4 +1,13 @@
 const { app } = require("express")();
+const sqlite = require("sqlite3").verbose();        //verbose makes debugging easier
 
+const db=  new sqlite.Database("./DevMedicos.db", (err)=>{
+    if(err){
+        console.log(err.message);  //API
+    }
+    else{
+        console.log("you are connected")  ;  //API
+    }
+})
 
 
